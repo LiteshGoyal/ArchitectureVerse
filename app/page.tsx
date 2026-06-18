588f75aeeccfbac6daf12228462caecc1c24da35
Sameer <liteshgoyal55@gmail.com>
2026-06-18 07:04:14 +0530

    updated imports

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import CTA from "./components/CTA";
 import FAQs from "./components/FAQs";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 export default function Home() {
   return (
